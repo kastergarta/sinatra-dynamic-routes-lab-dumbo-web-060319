32b3d5570c959e8de5a632d672bede28e8a4274a
Dennis <dennis.basyrov@gmail.com>
2019-06-25 12:52:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -41,17 +41,7 @@ class App < Sinatra::Base
       (@number1 / @number2).to_s
     else @operation == "multiply"
       (@number1 * @number2).to_s 
-    
-    
-    case @oper
-      when "subtract"
-        (@num1 - @num2).to_s
-      when "add"
-        (@num1 + @num2).to_s
-      when "multiply"
-        (@num1 * @num2).to_s
-      when "divide"
-        (@num1 / @num2).to_s
-  end
+    end
+  end 
 
 end
